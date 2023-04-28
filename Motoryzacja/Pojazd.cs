5a40da550048c312d509cf0eb54ad62af96a2d62
MichalCzajkowski1806 <103947663+MichalCzajkowski1806@users.noreply.github.com>
2023-04-28 13:56:21 +0200

    Initial commit

---
+++ Motoryzacja/Pojazd.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Motoryzacja
+{
+    struct RejestrNazw
+    {
+        DateTime DataModyfikacji;
+        string Nazwa;
+
+        public RejestrNazw(DateTime data, string nazwa)
+        {
+            this.DataModyfikacji = data;
+            this.Nazwa = nazwa;
+        }
+
+        public override string ToString()
+        {
+            return "data modyfikacji: " + DataModyfikacji + ", nazwa: " + Nazwa; 
+        }
+    }
+    public class Pojazd : IComparable<Pojazd>
+    {
+        public string Nazwa 
+        {
+            get { return this.nazwa; }
+            set
+            {
+                this.nazwa = value;
+                RejestrNazw staraData = new RejestrNazw(DateTime.Now, value);
+                HistoriaNazw.Add(staraData);
+            }
+        }
+        private string nazwa;
+        private int liczbaKol;
+        private double predkosc;
+        private static int liczbaPojazdow;
+        public int lp;
+        private List<RejestrNazw> HistoriaNazw = new List<RejestrNazw>();
+        public int CompareTo(Pojazd other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            return other.predkosc.CompareTo(predkosc);
+        }
+        public int Lp 
+        {
+            get { return this.lp; }
+            set { this.lp = value; } 
+        }
+        public int LiczbaKol
+        {
+            get { return this.liczbaKol; }
+            set
+            {
+                if (value < 2 || value > 8)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    this.liczbaKol = value;
+                }
+            }
+        }
+
+        public double Predkosc
+        {
+            get { return this.predkosc; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    this.predkosc = value;
+                }
+            }
+        }
+        public Pojazd()
+        {
+            liczbaPojazdow++;
+            this.Lp = liczbaPojazdow;
+            
+        }
+
+        static Pojazd()
+        {
+            liczbaPojazdow = 0;
+        }
+
+        public Pojazd(int liczbaKol, double predkosc, string nazwa): this()
+        {
+            this.LiczbaKol = liczbaKol; this.Predkosc = predkosc; this.Nazwa = nazwa;
+            
+        }
+        public Pojazd(double predkosc, string nazwa): this(4, predkosc, nazwa)
+        { 
+            this.Predkosc = predkosc; 
+            this.Nazwa = nazwa; 
+        }
+
+        public void WyswietlHistorię(ref ListBox listaHistorii)
+        {
+            foreach (var item in HistoriaNazw)
+            {
+                listaHistorii.Items.Add(item);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Nazwa: " + this.Nazwa + ", Prędkość: " + this.predkosc + "Km/h, Liczba kół: " + this.liczbaKol + ", Lp: " + this.lp + "/" + Pojazd.liczbaPojazdow;
+        }
+
+        public (T, T) Zamien<T>(T pierwszy, T drugi)
+        {
+            T pomocnicza = pierwszy;
+
+            pierwszy = drugi;
+
+            drugi = pomocnicza;
+            
+            return (pierwszy, drugi);
+        }
+    }
+
+    public class PojazdMechaniczny:Pojazd
+    {
+        private int mocSilnika;
+
+        public int MocSilnika
+        {
+            get { return this.mocSilnika; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    this.mocSilnika = value;
+                }
+            }
+        }
+
+        public PojazdMechaniczny(int liczbaKol, double predkosc, string nazwa, int mocsilnika):base(liczbaKol, predkosc, nazwa)
+        {
+            this.MocSilnika = mocsilnika;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", Moc silnika: " + this.mocSilnika + "KM";
+        }
+    }
+
+    public class Samochod:Pojazd
+    {
+        private int liczbaPasazerow;
+
+        public string Marka { get; set; }
+        public int LiczbaPasazerow
+        {
+            get { return this.liczbaPasazerow; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    this.liczbaPasazerow = value;
+                }
+            }
+        }
+
+        public Samochod(int liczbaKol, double predkosc, string nazwa, int liczbapasazerow, string marka):base(liczbaKol, predkosc, nazwa)
+        {
+            this.LiczbaPasazerow = liczbapasazerow;
+            this.Marka = marka;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", Pasażerowie: " + this.liczbaPasazerow + ", Marka: " + this.Marka;
+        }
+    }
+}
---
+++ Zadanie A/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ExtensionMethods;
+using Motoryzacja;
+
+namespace Zadanie_A
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void WyswietlPojazdy(IEnumerable<Pojazd> Pojazdy)
+        {
+            lstPojazdy.Items.Clear();
+
+            foreach (var pojazd in Pojazdy)
+            {
+                lstPojazdy.Items.Add(pojazd.ToString());
+            }
+        }
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Pojazd p1 = new Pojazd(2, 87, "Motorówka");
+                Pojazd p2 = new Pojazd(4, 124, "Osobówka");
+                Pojazd p3 = new Pojazd(6, 67, "Autobus");
+                Pojazd p4 = new Pojazd(4, 31, "Osobówka");
+                Pojazd p5 = new Pojazd(6, 26, "Dostawczak");
+                Pojazd p6 = new Pojazd(3, 86, "Motor");
+                Pojazd p7 = new Pojazd(4, 9, "Osobówka");
+                Pojazd p8 = new Pojazd(4, 44, "Osobówka");
+                Samochod s1 = new Samochod(4, 99, "Ciężarówka", 6, "Dacia");
+                Samochod s2 = new Samochod(4, 156, "Wyścigówka", 2, "Mercedes");
+                PojazdMechaniczny pm1 = new PojazdMechaniczny(4, 72, "Quad", 15);
+                //Pojazd[] tablica = new Pojazd[4] { p5, p6, p7, p8 };
+
+                List<Pojazd> pojazdy = new List<Pojazd>();
+                pojazdy.Add(p1);
+                pojazdy.Add(p2);
+                pojazdy.Add(p3);
+                pojazdy.Add(p4);
+                pojazdy.Add(p5);
+                pojazdy.Add(p6);
+                pojazdy.Add(p7);
+                pojazdy.Add(p8);
+                pojazdy.Add(s1);
+                pojazdy.Add(s2);
+                pojazdy.Add(pm1);
+                pojazdy.Sort();
+                
+                WyswietlPojazdy(pojazdy);
+                p1.Nazwa = "Traktor";
+                p1.Nazwa = "Czołg";
+               
+                p1.WyswietlHistorię(ref lstHistoriaNazw);
+                
+            }
+            catch(ArgumentException)
+            {
+                MessageBox.Show("Niepoprawne dane!");
+            }
+        }
+
+        private void btnExtension_Click(object sender, RoutedEventArgs e)
+        {
+            Pojazd p1 = new Pojazd(4, 55, "Auto");
+            PojazdMechaniczny pm1 = new PojazdMechaniczny(4, 88, "Osobówka", 120);
+            Samochod s1 = new Samochod(4, 190, "RS7", 2, "Audi");
+
+            p1.Wyswietl();
+            pm1.Wyswietl();
+            s1.Wyswietl();
+            MessageBox.Show(Convert.ToString(p1.Zamien<int>(5, 10)));
+        }
+    }
+}
+
+namespace ExtensionMethods
+{
+    public static class ExtensionMethod
+    {
+        public static void Wyswietl(this Pojazd p)
+        {
+            //if (p is Pojazd)
+            //{
+            //    MessageBox.Show(p.ToString() + ", rodzaj obiektu: Pojazd");
+            //}
+            //else if ((PojazdMechaniczny)p is PojazdMechaniczny)
+            //{
+            //    MessageBox.Show(p.ToString() + ", rodzaj obiektu: Pojazd mechaniczny");
+            //}
+            MessageBox.Show(p.ToString());
+        }
+    }
+}
+
+
